b87054f2e589fe49d02fab960d5e5307150d8110
cycasmi <lobo4wolf4@hotmail.com>
2018-12-03 15:43:35 -0600

    Final version - Weights updaTE

--- BPN_NN.cu
+++ BPN_NN.cu
@@ -66,6 +66,9 @@ __global__ void elemWise(float *A, float *B, float *C, const int opt, const int 
 
     	else if (opt == '*')
     		C[idx] = A[idx] * B[idx];
+
+    	else if (opt == 'm')
+    		C[idx] = B[1 * rows + ix_rows];
     }
 }
 
@@ -79,9 +82,42 @@ __global__ void costFunc(float *A, float *Y, float *cost, const int rows, const 
     	cost[0] += (-Y[idx] * log(A[idx])) - ((1 - Y[idx])*log(1 - A[idx]));
 }
 
-__global__ void predict(float *X, float *W1, float *W2, float *y, const int rows, const int cols)
+__global__ void cumulativeSum(float *A, float *result, const int rows, const int cols)
 {
+    unsigned int ix_rows = threadIdx.x + blockIdx.x * blockDim.x;
+    unsigned int iy_cols = threadIdx.y + blockIdx.y * blockDim.y;
+    unsigned int idx = iy_cols * rows + ix_rows;
 
+    if (ix_rows < rows && iy_cols < cols)
+    	result[iy_cols] += A[idx];
+
+    /* INTENTO DE HACER CON SHARED MEMORY
+    int sumIndex_X = threadIdx.x;
+    extern __shared__ float s_sum[];
+
+
+    if (ix_rows < rows && iy_cols < cols)
+    	s_sum[sumIndex_X] = A[idx];
+
+     __syncthreads();
+
+    // For reductions, threadsPerBlock must be a power of 2
+    int i = blockDim.x / 2;
+    while(i != 0)
+    {
+        if(sumIndex_X < i)
+            s_sum[sumIndex_X] += s_sum[sumIndex_X + i];
+        __syncthreads();
+        i /= 2;
+    }
+
+    
+    if (iy_cols < cols)
+    {
+    	if(sumIndex_X == 0)
+        	result[blockIdx.y] = s_sum[0];
+    }
+    */
 }
 
 int main(int argc, char *argv[])
@@ -163,22 +199,6 @@ int main(int argc, char *argv[])
 	for(int i = 0; i < hidden_layer_size * output_layer_size; i++)
 		w2.push_back(distribution(generator));
 
-	//std::cout << "Size: " << values_s.size()/400 << std::endl;
-	//std::cout << "Values: " << values.size()/400 << std::endl;
-
-	//std::cout << "Size: " << hidden_layer_size * output_layer_size << std::endl;
-	//std::cout << "W2: " << w2.size() << std::endl;
-
-	//std::cout << "Size: " << training_attr_size * hidden_layer_size << std::endl;
-	//std::cout << "W1: " << w1.size() << std::endl;
-
-	//Filling bias
-	//std::vector<float> b1(training_attr_size);
-	//std::vector<float> b2(hidden_layer_size);
-	//std::fill (b1.begin(), b1.end(), 1);
-	//std::fill (b2.begin(), b2.end(), 1);
-
-	//std::vector<float> z2(training_size * output_layer_size);
 
 	// Z2 will be the Results matrix
 	float *h_values, *h_labels, *h_w1, *h_w2, *h_z2, *h_outputs, *h_J;
@@ -192,20 +212,11 @@ int main(int argc, char *argv[])
 	for (int i = 0; i < training_size; i++)
 		h_outputs[(labels[i]-1) * training_size + i] = 1;
 
-	
-	//h_b1 = (float *)malloc(b1.size());
-	//h_b2 = (float *)malloc(b2.size());
-
 	h_values = values.data();
-	//h_labels = labels.data();
 	h_w1 = w1.data();
 	h_w2 = w2.data();
-	//h_z1 = z1.data();
-	//h_z2 = z2.data();
-	//h_b1 = b1.data();
-	//h_b2 = b2.data();
 
-	float *d_values, *d_labels, *d_w1, *d_w2, *d_z1, *d_z2, *d_b1, *d_b2, *d_outputs, *d_delta3, *d_delta2, *d_transW2, *d_J;
+	float *d_values, *d_labels, *d_w1, *d_w2, *d_z1, *d_z2, *d_b1, *d_b2, *d_outputs, *d_delta3, *d_delta2, *d_delta1, *d_transW2, *d_transW1, *d_J, *d_delta2Sum, *d_delta1Sum;
 	SAFE_CALL(cudaMalloc((void **)&d_values, values.size() * sizeof(float)), "Error allocating d_values");
 	SAFE_CALL(cudaMalloc((void **)&d_labels, labels.size() * sizeof(float)), "Error allocating d_labels");
 	SAFE_CALL(cudaMalloc((void **)&d_w1, w1.size() * sizeof(float)), "Error allocating d_w1");
@@ -219,6 +230,10 @@ int main(int argc, char *argv[])
 	SAFE_CALL(cudaMalloc((void **)&d_delta2, training_size * hidden_layer_size * sizeof(float)), "Error allocating d_delta2");
 	SAFE_CALL(cudaMalloc((void **)&d_transW2, output_layer_size * hidden_layer_size * sizeof(float)), "Error allocating d_transW2");
 	SAFE_CALL(cudaMalloc((void **)&d_J, 1 * sizeof(float)), "Error allocating d_J");
+	SAFE_CALL(cudaMalloc((void **)&d_delta2Sum, hidden_layer_size * sizeof(float)), "Error allocating d_delta2Sum");
+	SAFE_CALL(cudaMalloc((void **)&d_delta1, training_size * training_attr_size * sizeof(float)), "Error allocating d_delta1");
+	SAFE_CALL(cudaMalloc((void **)&d_delta1Sum, training_attr_size * sizeof(float)), "Error allocating d_delta1Sum");
+	SAFE_CALL(cudaMalloc((void **)&d_transW1, hidden_layer_size * training_attr_size * sizeof(float)), "Error allocating d_transW2");
 
 	// transfer data from host to device
 	SAFE_CALL(cudaMemcpy(d_values, h_values, values.size() * sizeof(float), cudaMemcpyHostToDevice), "Error copying d_values");
@@ -231,6 +246,10 @@ int main(int argc, char *argv[])
 	cudaMemset(d_delta2, 0, training_size * hidden_layer_size*sizeof(float));
 	cudaMemset(d_transW2, 0, output_layer_size * hidden_layer_size*sizeof(float));
 	cudaMemset(d_J, 0, 1*sizeof(float));
+	cudaMemset(d_delta2Sum, 0, hidden_layer_size*sizeof(float));
+	cudaMemset(d_delta1, 0, training_size * training_attr_size *sizeof(float));
+	cudaMemset(d_transW1, 0, hidden_layer_size * training_attr_size *sizeof(float));
+	cudaMemset(d_delta1Sum, 0, training_attr_size *sizeof(float));
     
 	// INVOKE KERNEL
 	int dimx = 32;
@@ -238,41 +257,126 @@ int main(int argc, char *argv[])
 	dim3 block(dimx, dimy);
 	dim3 grid((training_attr_size + block.x - 1) / block.x, (training_size + block.y - 1) / block.y);
 
-
-	std::cout << "\nForward Propagation\n";
-
+	//FORWARD PROP
+	std::cout << "\n *** Forward Propagation *** \n";
+	std::chrono::duration<float, std::milli> total_time = std::chrono::high_resolution_clock::duration::zero();
+	std::chrono::duration<float, std::milli> duration_ms = std::chrono::high_resolution_clock::duration::zero();
+	auto start_gpu =  std::chrono::high_resolution_clock::now();
  	matrixMult<<<grid, block>>>(d_values, d_w1, d_z1, training_size, hidden_layer_size);
  	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 1");
     SAFE_CALL(cudaGetLastError(), "Error with last error");
+    auto end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+	std::cout << "Matrix Mult (Values x W1) time: " << duration_ms.count() << "\n";
+	start_gpu =  std::chrono::high_resolution_clock::now();
  	matrixMult<<<grid, block>>>(d_z1, d_w2, d_z2, training_size, output_layer_size);
  	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 2");
     SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+	std::cout << "Matrix Mult (Z1 x W2) time: " << duration_ms.count() << "\n";
 
     //Calculating Cost
-    std::cout << "\nCalculating Cost\n";
+    std::cout << "\n *** Calculating Cost *** \n";
+    start_gpu =  std::chrono::high_resolution_clock::now();
     costFunc<<<grid, block>>>(d_z2, d_outputs, d_J, training_size, output_layer_size);
     SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel Cost");
     SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+	std::cout << "Cost calcultation time: " << duration_ms.count() << "\n";
+
     //SAFE_CALL(cudaMemcpy(h_J, d_J, 1 * sizeof(float), cudaMemcpyDeviceToHost), "CUDA Memcpy Device To Host Failed");
     //std::cout << "Costo: " << h_J[0] << std::endl;
 
     //BACKPROPAGATION
-    std::cout << "\nBackPropagation\n";
+    std::cout << "\n *** BackPropagation *** \n";
+    //Delta 3
+    start_gpu =  std::chrono::high_resolution_clock::now();
  	elemWise<<<grid, block>>>(d_z2, d_outputs, d_delta3, '-', training_size, output_layer_size);
  	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 3");
     SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+	std::cout << "Element wise operation time: " << duration_ms.count() << "\n";
+	start_gpu =  std::chrono::high_resolution_clock::now();
+	//Delta 2
     transpose<<<grid, block>>>(d_w2, d_transW2, hidden_layer_size, output_layer_size);
  	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 4");
     SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+	std::cout << "Transpose time: " << duration_ms.count() << "\n";
+	start_gpu =  std::chrono::high_resolution_clock::now();
     matrixMult<<<grid, block>>>(d_delta3, d_transW2, d_delta2, training_size, hidden_layer_size);
  	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 5");
     SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+    start_gpu =  std::chrono::high_resolution_clock::now();
     elemWise<<<grid, block>>>(d_delta2, d_z1, d_delta2, '*', training_size, hidden_layer_size);
  	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 6");
     SAFE_CALL(cudaGetLastError(), "Error with last error");
-
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+    start_gpu =  std::chrono::high_resolution_clock::now();
+    cumulativeSum<<<grid, block>>>(d_delta2, d_delta2Sum, training_size, hidden_layer_size);
+ 	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 7");
+    SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+	std::cout << "Cummulative Summation time: " << duration_ms.count() << "\n";
+	//Delta 1
+	start_gpu =  std::chrono::high_resolution_clock::now();
+    transpose<<<grid, block>>>(d_w1, d_transW1, training_attr_size, hidden_layer_size);
+ 	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 8");
+    SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+    matrixMult<<<grid, block>>>(d_delta2, d_transW1, d_delta1, training_size, training_attr_size);
+ 	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 9");
+    SAFE_CALL(cudaGetLastError(), "Error with last error");
+    start_gpu =  std::chrono::high_resolution_clock::now();
+    cumulativeSum<<<grid, block>>>(d_delta1, d_delta1Sum, training_size, training_attr_size);
+ 	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 10");
+    SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
 
     //Updating Weights
+    std::cout << "\n *** Weights update ***\n";
+    start_gpu =  std::chrono::high_resolution_clock::now();
+    elemWise<<<grid, block>>>(d_w2, d_delta2Sum, d_w2, 'm', hidden_layer_size, output_layer_size);
+ 	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 10");
+    SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+	std::cout << "W2 update time: " << duration_ms.count() << "\n";
+	start_gpu =  std::chrono::high_resolution_clock::now();
+    elemWise<<<grid, block>>>(d_w1, d_delta1Sum, d_w1, 'm', training_attr_size, hidden_layer_size);
+ 	SAFE_CALL(cudaDeviceSynchronize(), "Error executing kernel 11");
+    SAFE_CALL(cudaGetLastError(), "Error with last error");
+    end_gpu =  std::chrono::high_resolution_clock::now();
+	duration_ms = end_gpu - start_gpu;
+	total_time = total_time + duration_ms;
+	std::cout << "W1 update time: " << duration_ms.count() << "\n";
+
+	std::cout << "\nTotal time: " << total_time.count() << "\n";
+
+
+	return 0;	
+}
 
 
  	//float *h_w3;
@@ -282,17 +386,6 @@ int main(int argc, char *argv[])
  	//matrixMult<<<grid, block>>>(d_w2, d_z1, d_z2, output_layer_size, training_size, hidden_layer_size);
 
  	//SAFE_CALL(cudaMemcpy(h_z2, d_z2, z2.size(), cudaMemcpyDeviceToHost), "CUDA Memcpy Device to host Failed");
-
-	return 0;	
-}
-
-
-
-
-
-
-
-
 
 
 
